9fc6aab3cad5bfe02aa20cddeeb49aff3da8db12
valle1336 <7alexxi@gmail.com>
2024-05-21 14:05:42 +0200

    Routing and contect added to views

--- project1-frontend/src/App.js
+++ project1-frontend/src/App.js
@@ -8,12 +8,20 @@ import {
 } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
 import HomePage from "./views/HomePage";
+import About from './views/About';
+import Services from './views/Services';
+import Contact from './views/Contact';
+import Partners from './views/Partners';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
       <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/partners" element={<Partners />} />
     </Route>
   )
 );
