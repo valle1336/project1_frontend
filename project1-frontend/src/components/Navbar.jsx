67caf709b90cfc7aef42e4f4bf2130bb1d789cb0
valle1336 <7alexxi@gmail.com>
2024-05-22 17:01:08 +0200

    Imported css files

--- project1-frontend/src/components/Navbar.jsx
+++ project1-frontend/src/components/Navbar.jsx
@@ -1,31 +1,32 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar() {
-    return (
-      <nav>
-        <ul>
-            <li>
-                <h1>TMC</h1>
-            </li>
-            <li>
-                <Link to="/">Hem</Link>
-            </li>
-            <li>
-                <Link to="/about">Om oss</Link>
-            </li>
-            <li>
-                <Link to="/services">Tjänster</Link>
-            </li>
-            <li>
-                <Link to="/partners">Partners</Link>
-            </li>
-            <li>
-                <Link to="contact">Kontakt</Link>
-            </li>
-        </ul>
-      </nav>
-    )
+  return (
+    <nav>
+      <ul>
+        <li>
+          <h1>TMC</h1>
+        </li>
+        <li>
+          <Link to="/">Hem</Link>
+        </li>
+        <li>
+          <Link to="/about">Om oss</Link>
+        </li>
+        <li>
+          <Link to="/services">Tjänster</Link>
+        </li>
+        <li>
+          <Link to="/partners">Partners</Link>
+        </li>
+        <li>
+          <Link to="contact">Kontakt</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
